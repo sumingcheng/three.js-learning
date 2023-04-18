30f777079fbc85e023a8a2517b2fbc442d491b35
范晓豪 <fanxiaohaohaha@qq.com>
2023-04-18 16:53:26 +0800

    loading.js

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@
 
 <template>
   <div>
-    <h1>
-      11111
-    </h1>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/style/index.less'
+import '@/animation/loading.js'
 
 createApp(App).mount('#app')
