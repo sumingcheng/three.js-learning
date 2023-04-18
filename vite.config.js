6e6c7ae23178c90cd9bfcb57e8683d14152f5b44
范晓豪 <fanxiaohaohaha@qq.com>
2023-04-18 16:38:39 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     // 设置自动打开浏览器
     open: true
+  },
+  resolve: {
+    alias: {
+      // 使用 path.join() 替代 path.resolve()
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve('src/components'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 可以在这里配置 less-loader 的选项
+        javascriptEnabled: true,
+      },
+    }
   }
 })
